996469f3c656cc3ade067d4d7203c8193d7b16aa
PranshuBasak <75575986+PranshuBasak@users.noreply.github.com>
2023-08-02 19:56:23 +0530

    Adding files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const hex = document.querySelector('.hex');
+const buttons = document.querySelector(".generator");
+
+
+
+buttons.addEventListener("click", generators)
+
+
+function generators(){
+    const hexcolor = Math.random().toString(16).substring(2,8);
+    console.log(hexcolor);
+    document.body.style.backgroundColor = "#" + hexcolor;
+    hex.innerText = "#" + hexcolor;
+}
+
+generators()
+
+
+
+
+
+
+
+
+let color = Math.random().toString(16); // 16 defines base of 16 which also means 0 to 9 number and 10 to 25 it resembles alphabets that is a to f.
