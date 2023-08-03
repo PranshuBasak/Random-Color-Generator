dbdbe6f6b7fe46bb7e2565c8abc9c28031ee3add
PranshuBasak <pranshubasak@gmail.com>
2023-08-03 20:30:03 +0530

    push pratice 2

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 const hex = document.querySelector('.hex');
 const buttons = document.querySelector(".generator");
-
-
-
 buttons.addEventListener("click", generators)
-
-
 function generators(){
     const hexcolor = Math.random().toString(16).substring(2,8);
     console.log(hexcolor);
@@ -13,18 +8,5 @@ function generators(){
     hex.innerText = "#" + hexcolor;
 }
 
-
-
-
-
-
 generators()
-
-
-
-
-
-
-
-
 let color = Math.random().toString(16); // 16 defines base of 16 which also means 0 to 9 number and 10 to 25 it resembles alphabets that is a to f.
